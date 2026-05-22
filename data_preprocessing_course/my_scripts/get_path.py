3c21fda71536960dbaec52872cbe66e0cec074e1
Rihaan1344 <syedmrihaan@gmail.com>
2026-05-22 12:18:27 +0530

    changed file structure so that sections are seperated

--- data_preprocessing_course/my_scripts/get_path.py
+++
@@ -1,3 +0,0 @@
-import os
-def get_path(bins = False) -> str:
-    return os.path.join(os.path.dirname(os.getcwd()), "resources", ("sample_dataset.csv" if not bins else "sample_dataset_bins.csv"))
---
+++ data_preprocessing_course/my_scripts/section 2 - data cleaning/get_path.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+def get_path(bins = False) -> str:
+    return "/Users/rihaan/Desktop/repos/smart-snakes/data_preprocessing_course/resources/sample_dataset.csv" if not bins else "/Users/rihaan/Desktop/repos/smart-snakes/data_preprocessing_course/resources/sample_dataset_bins.csv"
